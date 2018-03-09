9bd25b60acb90d05747d539f1357e46a96e1e305
hk <ddd>
2018-03-09 18:47:19 +0100

    tes

--- src/main/java/hk/test/javaApp1.java
+++ src/main/java/hk/test/javaApp1.java
@@ -4,7 +4,11 @@ public class javaApp1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.print("Java app executed!");
 	}
 
+	public void testMethod1() {
+		
+		System.out.print("test");
+	}
 }
